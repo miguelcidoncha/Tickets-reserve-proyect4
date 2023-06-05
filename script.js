80a731742711e856454a2159efb36365f2c0651f
miguelcidoncha <cidoncha1996@gmail.com>
2023-06-05 18:07:07 +0200

    Validate-form

--- script.js
+++ script.js
@@ -16,3 +16,42 @@ function calculatePrice() {
     });
     document.getElementById("total").value = totalFormat;
 }
+
+
+function validateForm() {
+    let nameInput = document.getElementById("name");
+    let nameValue = nameInput.value.trim();
+    let emailInput = document.getElementById("email");
+    let emailValue = emailInput.value.trim();
+    let phoneInput = document.getElementById("phone");
+    let phoneValue = phoneInput.value.trim();
+
+    let numberPattern = /[0-9]/;
+    let phonePattern = /^[0-9]+$/;
+
+    let validForm = true;
+
+
+    if (numberPattern.test(nameValue)) {
+        let nameError = document.getElementById("name-error");
+        nameError.textContent = "El nombre y apellidos no deben contener números";
+        nameError.style.display = "inline-block";
+        validForm = false;
+    } else {
+        let nameError = document.getElementById("name-error");
+        nameError.style.display = "none";
+    }
+
+
+    if (!phonePattern.test(phoneValue)) {
+        let phoneError = document.getElementById("phone-error");
+        phoneError.textContent = "El teléfono debe contener solo números";
+        phoneError.style.display = "inline-block";
+        validForm = false;
+    } else {
+        let phoneError = document.getElementById("phone-error");
+        phoneError.style.display = "none";
+    }
+
+    return validForm;
+}
