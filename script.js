07533ef3be475a3c8db5a63f4e96ae58fee5bf14
miguelcidoncha <cidoncha1996@gmail.com>
2023-06-05 17:06:12 +0200

    CalculatePrice ()

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 function calculatePrice() {
     let price = document.getElementById("price").value;
-    let adults = document.getElementById("adults").value;
     let tickets = document.getElementById("tickets").value;
-    let restaurant = document.getElementById("restaurant").value;
-    let subtotalAdults = price * adults;
-    let subtotalTicketsRestaurant = tickets * restaurant;
-    let total = subtotalAdults + subtotalTicketsRestaurant;
+    let children = document.getElementById("children").value;
+    let todayM = document.getElementById("today's menu").value;
+    let childrenM = document.getElementById("Children's menu").value;
+    let buffet = document.getElementById("buffet").value;
+    let days = document.getElementById("days").value;
+
+    let menuTotal = (Number(todayM) * 15) + (Number(childrenM) * 10) + (Number(buffet) * 20);
+    let total = (price * tickets - (price * children) + menuTotal) * days;
+
     let totalFormat = total.toLocaleString("es-ES", {
         style: "currency",
         currency: "EUR",
